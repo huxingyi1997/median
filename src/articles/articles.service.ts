ee998604984d2eca39959c7427358d5db22376c2
xingyi.hu <xingyi.hu@shopee.com>
2024-01-23 15:51:55 +0800

    feat: init

---
+++ src/articles/articles.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+
+@Injectable()
+export class ArticlesService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createArticleDto: CreateArticleDto) {
+    return await this.prisma.article.create({ data: createArticleDto });
+  }
+
+  async findAll() {
+    return await this.prisma.article.findMany({ where: { published: true } });
+  }
+
+  async findDrafts() {
+    return await this.prisma.article.findMany({ where: { published: false } });
+  }
+
+  async findOne(id: number) {
+    return await this.prisma.article.findUnique({ where: { id } });
+  }
+
+  async update(id: number, updateArticleDto: UpdateArticleDto) {
+    return await this.prisma.article.update({
+      where: { id },
+      data: updateArticleDto,
+    });
+  }
+
+  async remove(id: number) {
+    return await this.prisma.article.delete({ where: { id } });
+  }
+}
